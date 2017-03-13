29b59d098ab95951166fa7dbaa6a8dd9c82ac848
Jim Barrows <Jim.Barrows@gmail.com>
2017-03-12 18:03:50 -0700

    Position type classification

--- 02-create-database.sql
+++ 02-create-database.sql
@@ -33,3 +33,19 @@ create table if not exists position_responsibility(
   defined_by uuid not null references responsibility_type(id),
   CONSTRAINT position_responsibility_pk PRIMARY key(id)
 );
+
+create table if not exists position_classification_type(
+  id uuid DEFAULT uuid_generate_v4(),
+  description text not null CONSTRAINT position_classification_type_description_not_empty CHECK (description <> ''),
+  CONSTRAINT position_classification_type_pk PRIMARY key(id)
+);
+
+create table if not exists position_type_class(
+  id uuid DEFAULT uuid_generate_v4(),
+  from_date date not null default current_date,
+  thru_date date,
+  standard_hours_per_week bigint not null default 40,
+  for_position_type uuid not null references position_type (id),
+  defined_by uuid not null references position_classification_type(id),
+  CONSTRAINT _pk PRIMARY key(id)
+);
